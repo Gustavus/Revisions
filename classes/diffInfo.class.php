b02252d35ac74fc073b867ef916615794db2f289
Billy Visto <bvisto@gustavus.edu>
2012-01-26 13:31:16 -0600

    added diffInfo class for diffInfo objects

---
+++ classes/diffInfo.class.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * @package Revisions
+ */
+namespace Gustavus\Revisions;
+require_once 'revisions/classes/revisionsBase.class.php';
+
+/**
+ * Contains information on how to roll back each specific part of a revision
+ *
+ * @package Revisions
+ */
+class DiffInfo extends RevisionsBase
+{
+  /**
+   * @var mixed index change starts
+   */
+  protected $startIndex;
+
+  /**
+   * @var mixed index change ends
+   */
+  protected $endIndex;
+
+  /**
+   * @var mixed content revision contained
+   */
+  protected $revisionInfo;
+
+  /**
+   * Class constructor
+   *
+   * @param array $params
+   */
+  public function __construct(array $params = array())
+  {
+    $this->populateObjectWithArray($params);
+  }
+
+  /**
+   * Class destructor
+   *
+   * @return void
+   */
+  public function __destruct()
+  {
+    unset($this->startInder, $this->endIndex, $this->revisionInfo);
+  }
+
+  /**
+   * @return mixed
+   */
+  public function getStartIndex()
+  {
+    if ($this->startIndex === null) {
+      return $this->startIndex;
+    } else {
+      return (int) $this->startIndex;
+    }
+  }
+
+  /**
+   * @return mixed
+   */
+  public function getEndIndex()
+  {
+    if ($this->endIndex === null) {
+      return $this->endIndex;
+    } else {
+      return (int) $this->endIndex;
+    }
+  }
+
+  /**
+   * @return mixed
+   */
+  public function getRevisionInfo()
+  {
+    return $this->revisionInfo;
+  }
+}
---
+++ tests/diffInfo.class.Test.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * @package Revisions
+ * @subpackage Tests
+ */
+
+namespace Gustavus\Revisions\Test;
+use Gustavus\Revisions;
+
+require_once '/cis/lib/test/test.class.php';
+require_once 'revisions/classes/diffInfo.class.php';
+
+/**
+ * @package Revisions
+ * @subpackage Tests
+ */
+class DiffInfoTest extends \Gustavus\Test\Test
+{
+  /**
+   * @var \Gustavus\Revisions\Revision
+   */
+  private $diffInfo;
+
+  /**
+   * @var array to fill DiffInfo objectWith
+   */
+  private $diffInfoProperties = array(
+    'startIndex' => 1,
+    'endIndex'  => null,
+    'revisionInfo' =>' testing',
+  );
+
+  /**
+   * sets up the object for each test
+   * @return void
+   */
+  public function setUp()
+  {
+    $this->diffInfo = new Revisions\DiffInfo($this->diffInfoProperties);
+  }
+
+  /**
+   * destructs the object after each test
+   * @return void
+   */
+  public function tearDown()
+  {
+    unset($this->diffInfo, $this->diffInfoProperties);
+  }
+
+  /**
+   * @test
+   */
+  public function getStartIndex()
+  {
+    $this->assertSame($this->diffInfoProperties['startIndex'], $this->diffInfo->getStartIndex());
+  }
+
+  /**
+   * @test
+   */
+  public function getEndIndex()
+  {
+    $this->assertSame($this->diffInfoProperties['endIndex'], $this->diffInfo->getEndIndex());
+  }
+
+  /**
+   * @test
+   */
+  public function getRevisionInfo()
+  {
+    $this->assertSame($this->diffInfoProperties['revisionInfo'], $this->diffInfo->getRevisionInfo());
+  }
+}
