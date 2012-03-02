9b2e721be415cc48073347ceeca2325c5a74fc7d
Billy Visto <bvisto@gustavus.edu>
2012-03-02 09:09:05 -0600

    Updated limits to make the rendering more consistent and removed code that was no longer needed

--- RevisionsRenderer.php
+++ RevisionsRenderer.php
@@ -79,26 +79,16 @@ class RevisionsRenderer
    */
   public function renderRevisions($limit = null, $oldestRevNum = null)
   {
-    if ($limit === null) {
-      $limit = $this->revisions->getLimit();
+    if ($limit !== null) {
+      $this->revisions->setLimit($limit);
     }
     if ($oldestRevNum === null) {
-      // +1 so we pull in one more revision for calculating the added/removed bytes
-      $this->revisions->setLimit($limit + 1);
       $this->revisions->populateEmptyRevisions();
-      // reset limit
-      $this->revisions->setLimit($limit);
     } else {
-      // -1 so we pull in one more revision for calculating the added/removed bytes
-      $this->revisions->populateEmptyRevisions($oldestRevNum - 1);
+      $this->revisions->populateEmptyRevisions($oldestRevNum);
     }
     $oldestRevNumPulled = $this->revisions->findOldestRevisionNumberPulled();
-    if ($oldestRevNumPulled !== null && $oldestRevNumPulled > 0 && !$this->revisions->revisionsHaveErrors()) {
-      $moreRevisionButton = $oldestRevNumPulled;
-    } else {
-      $moreRevisionButton = 0;
-    }
-    return $this->renderTwig('revisions.twig', null, array('oldestRevisionNumber' => $moreRevisionButton), $oldestRevNumPulled);
+    return $this->renderTwig('revisions.twig', null, array(), $oldestRevNumPulled);
   }
 
   /**
@@ -112,6 +102,8 @@ class RevisionsRenderer
    */
   public function renderRevisionComparisonText($oldRevNum, $newRevNum, $column = null, $oldestRevNum = null)
   {
+    // - 1 so we pull in one more revision for rendering content changes
+    $this->revisions->populateEmptyRevisions(min($oldRevNum, $newRevNum) - 1);
     return $this->renderTwig('revisionDataText.twig', $this->revisions->compareTwoRevisions($oldRevNum, $newRevNum, $column), array('visibleRevisions' => array($oldRevNum, $newRevNum)), $oldestRevNum);
   }
 
@@ -125,6 +117,8 @@ class RevisionsRenderer
    */
   public function renderRevisionData($revNum, $column = null, $oldestRevNum = null)
   {
+    // - 1 so we pull in one more revision for rendering content changes
+    $this->revisions->populateEmptyRevisions($revNum - 1);
     return $this->renderTwig('revisionData.twig', $this->revisions->getRevisionByNumber($revNum, $column), array('visibleRevisions' => array($revNum)), $oldestRevNum);
   }
 
@@ -142,18 +136,11 @@ class RevisionsRenderer
   /**
    * Renders out a table of revisionData for each column with a button to undo action
    *
-   * @param  integer oldestRevNum oldestRevNum pulled into the revisions Object
    * @return string
    */
-  public function renderRevisionThankYou($oldestRevNum)
+  public function renderRevisionThankYou()
   {
-    $limit = $this->revisions->getLimit();
-    $this->revisions->setLimit(1);
-    $this->revisions->populateEmptyRevisions();
-    $revNum = $this->revisions->findLatestRevisionNumberPulled();
-    // set limit to what the application initially set it to be
-    $this->revisions->setLimit($limit);
-    return $this->renderTwig('revisionThankYou.twig', $this->revisions->getRevisionByNumber($revNum), array('visibleRevisions' => array($revNum)), $oldestRevNum);
+    return $this->renderTwig('revisionThankYou.twig', null);
   }
 
   /**
@@ -182,8 +169,9 @@ class RevisionsRenderer
    */
   private function renderTwig($filename, $revision, array $params = array(), $oldestRevNum = null)
   {
-    $oldestRevisionNumber = ($oldestRevNum === null) ? $this->revisions->findOldestRevisionNumberPulled() - 1 : $oldestRevNum;
+    $oldestRevisionNumber = ($oldestRevNum === null) ? $this->revisions->findOldestRevisionNumberPulled() : $oldestRevNum - 1;
     $params = array_merge(
+        $params,
         array(
           'revision'             => $revision,
           'revisions'            => $this->revisions->getRevisionObjects($oldestRevisionNumber),
@@ -196,8 +184,7 @@ class RevisionsRenderer
           'limit'           => $this->revisions->getLimit(),
           'maxColumnSizes'  => $this->revisions->getMaxColumnSizes(),
           'fullUrl'         => $this->makeUrl($this->removeParams($this->revisionsUrlParams, array('oldestRevisionNumber'))),
-        ),
-        $params
+        )
     );
     return \Gustavus\TwigFactory\TwigFactory::renderTwigFilesystemTemplate("/cis/lib/Gustavus/Revisions/views/$filename", $params);
   }
