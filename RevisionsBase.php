1d7c82ea101e3080e733430f5e7250e35be245be
Billy Visto <bvisto@gustavus.edu>
2012-03-28 09:01:23 -0500

    added functions to convert a string to a number that way we are always dealing with a number's actual type if it got saved as a string from the application

--- RevisionsBase.php
+++ RevisionsBase.php
@@ -12,6 +12,8 @@ namespace Gustavus\Revisions;
 abstract class RevisionsBase
 {
   /**
+   * Populate object with an associative array.
+   *
    * @param array $array
    * @return void
    */
@@ -21,6 +23,36 @@ abstract class RevisionsBase
       if (property_exists($this, $key)) {
         $this->$key = $value;
       }
+    }
+  }
+
+  /**
+   * Convert number to its respective type
+   *
+   * @param  mixed $number
+   * @return mixed         either an integer or float
+   */
+  protected function toNumber($number)
+  {
+    if (gettype($number) === 'string') {
+      return $this->stringToNumber($number);
+    } else {
+      return $number;
+    }
+  }
+
+  /**
+   * Convert a string to an integer or float depending on if it has a decimal or not
+   *
+   * @param  string $number
+   * @return mixed         either an integer or float
+   */
+  protected function stringToNumber($number)
+  {
+    if (preg_match('`.*\.[\d]+$`', $number) !== 0) {
+      return (float) $number;
+    } else {
+      return (int) $number;
     }
   }
 }
